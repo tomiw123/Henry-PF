380d779bc65903f3bcb8860dc0e8d0f354dfa431
tomiw123 <tomacowalker@gmail.com>
2022-11-23 18:33:32 -0300

    reacomodo de carpetas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        <h1>hooloas</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
