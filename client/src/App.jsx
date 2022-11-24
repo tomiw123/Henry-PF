d81c6d287cd54f86b8ea2b866ae9a2d5ad9ea299
francisco1031 <francisco10_31@hotmail.com>
2022-11-24 20:59:56 -0300

    primera vista de tarjeta

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import './App.css'
 import Register from './page/NavbarPege/Register'
 import {Route, Routes} from 'react-router-dom'
 
-import Home from './page/NavbarPege/Home'
+//import Home from './page/NavbarPege/Home'
+import Home from './component/Home/Home'
 
 function App() {
   return(
--- client/src/component/Card/Card.jsx
+++ client/src/component/Card/Card.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import style from './Card.module.css'
 
 
 export default function Card(props){
     return (
-        <div>
-            <img />
-            <h1> </h1>
-        </div>
+        <div className={style.card}>
+       
+        <img src={props.img} alt={props.name}  />
+           
+        <h1>{props.name}</h1>
+        
+        <h3>${props.precio}</h3>
+        
+      </div>
     )
 }
