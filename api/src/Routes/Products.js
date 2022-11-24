1a724349bbada62a1fff0c30f57a6f909a69e320
vicmen86 <victrinsumos@gmail.com>
2022-11-24 13:11:36 -0300

    Rutas product y users

--- api/src/Routes/Products.js
+++ api/src/Routes/Products.js
@@ -1,14 +1,79 @@
-const {Router} = require('express')
+const { Router } = require("express");
 const router = Router();
-const Product = require('../Models/Product.js');
+const Product = require("../Models/Product.js");
 
-router.get('/', async(req,res) =>{
-    try{
-      const products = await Product.find();
-      res.status(200).json(products);
-    }catch(err){
-        console.log('no funco')
+
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { name, price, image, description } = req.body;
+  console.log(name);
+  try {
+    const exist = await Product.findOne({ name });
+    if (!exist) {
+      const addProduct = await Product.create({
+        name,
+        price,
+        image,
+        description,
+      });
+      res.status(200).send(`Product ${name} created`);
+    } else {
+      res.status(404).send("El producto ya existe");
     }
-})
+  } catch (err) {
+    console.log("no funco");
+  }
+});
+
+router.put("/:_id", async (req, res) => {
+  const { _id } = req.params;
+  const { category } = req.body;
+  console.log(category);
+  try {
+    const product = await Product.findByIdAndUpdate(
+      _id,
+      { $push: { category } },
+      { useFindAndModify: false }
+    );
+    res.status(200).send(product);
+  } catch (err) {
+    console.log("no funco");
+  }
+});
+// eliminar una categoria
+router.put("/eliminar/:_id", async (req, res) => {
+  const { _id } = req.params;
+  const { catego } = req.body;
+  try {
+    const productUpdated = await Product.findByIdAndUpdate(_id, {
+    $set: { category: []}
+  },{
+    useFindAndModify:false
+  });
+  res.status(200).send(productUpdated);
+  } catch (error) {
+    console.log(error);
+  } 
+});
+
+router.delete("/:_id", async (req, res) => {
+  const {_id}=req.params
+
+  try {
+    const products = await Product.findByIdAndDelete(_id);
+    res.status(200).send(products.name);
+    
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
--- api/src/Routes/Users.js
+++ api/src/Routes/Users.js
@@ -11,6 +11,16 @@ router.get('/', async(req,res) =>{
     }
 })
 
-router
+router.post('/', async(req,res) =>{
+  const {name, contact, adress, pass}=req.body
+  try{
+    const users = await User.create({
+      name, contact, adress, pass,
+    });
+    res.status(200).json(users);
+  }catch(err){
+      console.log('no hay usuarios')
+  }
+})
 
 module.exports = router;
