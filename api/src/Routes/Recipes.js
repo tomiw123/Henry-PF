74c5d2a2e5dcf4ae67a0ca93cf33dece399008e4
hugoherediajr <hhhaarryy@gmail.com>
2022-11-28 19:37:01 -0300

    controlers al 100

--- api/src/Routes/Recipes.js
+++ api/src/Routes/Recipes.js
@@ -1,7 +1,6 @@
 const {Router} = require('express')
 const router = Router();
 const {
-<<<<<<< HEAD
     getRecipes,
     createRecipe,
     deleteRecipe,
@@ -11,16 +10,6 @@ router.get('/', getRecipes);
 router.post('/', createRecipe);
 router.delete('/:_id', deleteRecipe)
 
-=======
-  getRecipes,
-  deleteRecipe,
-  createRecipe
-} = require('../Controllers/Recipe')
-
-router.get('/',getRecipes )
-router.get('/',deleteRecipe )
-router.get('/',createRecipe )
->>>>>>> 165c741e3b32e931ec3df6ae63ff6fe0a9da52d4
 
 
 module.exports = router;
