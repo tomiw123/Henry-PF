d3e2e4e3d4d2e013426da6caa22d056c612b4cb0
lceras <luisfcss1102@hotmail.com>
2022-11-27 20:23:54 -0500

    solo por un espacio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,27 @@
-import CreateProduct from './component/CreateProduct/CreateProduct'
+import CreateProduct from "./component/CreateProduct/CreateProduct";
 import "./App.css";
 import Register from "./page/NavbarPege/Register";
 import { Route, Routes } from "react-router-dom";
-import Login from "./page/NavbarPege/Login"
-import Reset from "./page/NavbarPege/Reset"
+import Login from "./page/NavbarPege/Login";
+import Reset from "./page/NavbarPege/Reset";
 
 import Home from "./component/Home/Home";
-import Nav from './component/Navbar/Nav';
-import SearchBar from './component/SearchBar/SearchBar';
+import Nav from "./component/Navbar/Nav";
+import SearchBar from "./component/SearchBar/SearchBar";
 //import Carrousel from './component/Carrousel/Carrousel';
-
-
-
 
 function App() {
   return (
     <div className="h-screen">
-      <Nav/>
+      <Nav />
       <SearchBar />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
         <Route path="login" element={<Login />} />
-          <Route path="reset" element={<Reset />} />
-        <Route path="/create" element={<CreateProduct/>} />
+        <Route path="reset" element={<Reset />} />
+        <Route path="/create" element={<CreateProduct />} />
       </Routes>
-     
     </div>
   );
 }
