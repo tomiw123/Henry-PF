c84bde8bcd81977ed2cdb874be29ba2b9dc3d662
tomiw123 <tomacowalker@gmail.com>
2022-12-01 01:16:47 -0300

    setDoc DB, firebase

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 import {getAuth} from "firebase/auth";
 const {
   VITE_API_KEY,
@@ -26,5 +27,6 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
 const analytics = getAnalytics(app);
 export const auth = getAuth(app)
