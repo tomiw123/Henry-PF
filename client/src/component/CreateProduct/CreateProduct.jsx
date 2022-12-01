eeb99bc74928692d8807ebe514d2a4a314d6dbc4
Matias Carballo <matiascarballo03@hotmail.com>
2022-12-01 20:32:37 -0300

    c

--- client/src/component/CreateProduct/CreateProduct.jsx
+++ client/src/component/CreateProduct/CreateProduct.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import { addProducts } from '../../redux/actions/actions'
 import { useDispatch } from 'react-redux';
 
-import { Link } from 'react-router-dom';
-import style from '../../component/CreateProduct/Create.module.css'
 import { Navigate } from 'react-router-dom';
 
 import { Formik, Form, Field, ErrorMessage } from 'formik';
