0b7f58a691566ef0fc0a6f1dc62ebcab21096bb0
lceras <luisfcss1102@hotmail.com>
2022-12-01 13:30:51 -0500

    mas cambios al form contacto

--- client/src/component/Contacto/Contacto.jsx
+++ client/src/component/Contacto/Contacto.jsx
@@ -1,52 +1,81 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import style from "./Contacto.module.css";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { FaWhatsapp } from "react-icons/fa";
+import emailjs from "@emailjs/browser";
+const { VITE_SERVICE_ID, VITE_TEMPLATE_ID, VITE_PUBLIC_KEY } = import.meta.env;
 
 const Contacto = () => {
+  const form = useRef()
+  console.log(form);
+  const sendEmail = (e) => {
+
+    emailjs
+      .sendForm(
+        VITE_SERVICE_ID,
+        VITE_TEMPLATE_ID,
+        form.current,
+        VITE_PUBLIC_KEY
+      )
+      .then((result) => console.log(result))
+      .catch((error) => console.log(error));
+  };
+
   return (
+    // <div>
+    //   <form ref={form} onSubmit={sendEmail}>
+        
+    //   </form>
+    // </div>
+    
     <Formik
       initialValues={{
-        client: "",
-        comments: "",
-        name: "",
+        user_email: "",
+        user_comments: "",
+        user_name: "",
       }}
       validationSchema={Yup.object({
-        name: Yup.string()
+        user_name: Yup.string()
           .max(15, "Must be 20 characters or less")
           .required("Name is required"),
-        client: Yup.string()
+        user_email: Yup.string()
           .email("Invalid email address")
           .max(40, "Must be 20 characters or less")
-          .required("Client is required"),
-        comments: Yup.string().required("Comments is required"),
+          .required("Email is required"),
+        user_comments: Yup.string().required("Comments is required"),
       })}
+
       onSubmit={(values, actions) => {
         // console.log(values);
         alert(JSON.stringify(values, null, 2));
-        actions.setSubmitting(false);
+        actions.setSubmitting(true);
         actions.resetForm();
+        actions.submitForm()
+        sendEmail()
       }}
+
+      // onSubmit={sendEmail}
+      innerRef={form}
     >
       <div className={style.principal}>
         <div className={style.card}>
           <h1>Contactenos</h1>
-          <Form>
+          <Form >
             <label>Ingresa tu Nombre</label>
-            <Field type="text" name="name" />
-            <ErrorMessage name="name" />
+            <Field type="text" name="user_name" />
+            <ErrorMessage name="user_name" />
             <label>Ingresa tu Email</label>
-            <Field type="email" name="client" />
-            <ErrorMessage name="client" />
+            <Field type="email" name="user_email" />
+            <ErrorMessage name="user_email" />
             <label>Deja tus comentarios</label>
-            <Field rows="3" cols="3" name="comments" as="textarea" />
-            <ErrorMessage name="comments" />
+            <Field rows="3" cols="3" name="user_comments" as="textarea" />
+            <ErrorMessage name="user_comments" />
             <button type="submit">enviar</button>
           </Form>
         </div>
       </div>
     </Formik>
+  
   );
 };
 
