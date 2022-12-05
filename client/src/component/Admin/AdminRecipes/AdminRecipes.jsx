f60c3e555160e567a7f7e70c6a978e71c6c2364a
Matias Carballo <matiascarballo03@hotmail.com>
2022-12-05 10:35:26 -0300

    correcciones

--- client/src/component/Admin/AdminRecipes/AdminRecipes.jsx
+++ client/src/component/Admin/AdminRecipes/AdminRecipes.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import style from '../AdminRecipes/AdminRecipes.module.css'
 import AdminRecipe from '../AdminRecipe/AdminRecipe'
-import Recipes from '../../Recipes/Recipes.json'
+//import Recipes from '../../Recipes/Recipes.json'
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllRecipes } from '../../../redux/actions/recipesActions';
 import { useEffect } from 'react';
+import Paginado from "../../Paginado/Paginado";
 
 const AdminRecipes = () => {
     const dispatch = useDispatch();
     useEffect(()=>{dispatch(getAllRecipes())},[dispatch])
     const Recipes = useSelector(state => state.recipes.docs);
+    const recipes = useSelector(state => state.recipes);
     console.log(Recipes);
     return (
         <div className={style.container}>
             {
-                Recipes.map((p) => {
+                Recipes?.map((p) => {
                     return(
-                        <div key={p._id}>
+                
                     <AdminRecipe key={p.id} name ={p.name} image={p.image} />
-                    </div>
+                    
                 )
             })
             }
+            <div className={style.paginado}>
+            <Paginado recipes={recipes} />
+            </div>
         </div>
     );
 };
