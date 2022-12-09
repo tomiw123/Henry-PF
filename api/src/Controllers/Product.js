c9cbb94c7fcb3742b0f614b6c7bcbd870b91c58e
vicmen86 <victrinsumos@gmail.com>
2022-12-09 10:54:38 -0300

    filtrado y reacomodo de las rutas de productos y filtrado

--- api/src/Controllers/Product.js
+++ api/src/Controllers/Product.js
@@ -1,53 +1,64 @@
 const Product = require("../Models/Product.js");
 
 const getAll = async (req, res) => {
-  //search, filter and paginate
-  const {search, filter, category, price, alfa } = req.query;
+  //search and paginate
+  const { search } = req.query;
   const limit = req.query.limit || 8;
   const page = req.query.page || 1;
- //filter: categorias(cat) precio(price) alfabeticamente(alfa) fecha creado(create)
- //category: categorias disponibles //price:1 y -1 //alfa:1 y -1 
- 
+
   try {
-    if (search){
-      const products = await Product.paginate({ 
+    if (search) {
+      const products = await Product.paginate({
         name: { $regex: ".*" + search + ".*", $options: "i" },
       });
       res.status(200).json(products);
-    }else if(filter){
-      if (filter == "cat") {
-        const products = await Product.paginate({ category },{limit, page });
-        res.status(200).json(products);
-      }
-      if (filter == "price") {
-        const products = await Product.paginate({}, { sort: { price: 1 }, limit, page });
-        res.status(200).json(products);
-      }
-      if (filter == "alfa") {
-        const products = await Product.paginate({},{sort: { name: 1 }, limit, page });
-        res.status(200).json(products);
-      }
-    }else {
+    } else {
       const products = await Product.paginate({}, { limit, page });
       res.status(200).json(products);
     }
   } catch (err) {
     console.log(err);
   }
-};    
-  
+};
+const getAllFilter = async (req, res) => {
+  //filter: categorias(category) precio(price) alfabeticamente(alfa) fecha creado(create)
+  //category: categorias disponibles //price:1 y -1 //alfa:1 y -1
+  const { filter, category, price, alfa } = req.query;
+  const limit = req.query.limit || 8;
+  const page = req.query.page || 1;
+
+  try {
+    if (filter) {
+      if (filter == "category") {
+        const products = await Product.paginate({ category }, { limit, page });
+        res.status(200).json(products);
+      }
+      if (filter == "price") {
+        const products = await Product.paginate({}, { limit, page, sort: { price } });
+        res.status(200).json(products);
+      }
+      if (filter == "alfa") {
+        const products = await Product.paginate({},{ limit, page, sort: { name: alfa } });
+        res.status(200).json(products);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("No hay productos para mostrar")
+  }
+};
+
 // producto por id
 const getId = async (req, res) => {
   const { id } = req.params;
-  console.log(id)
+  console.log(id);
   try {
-    const products = await Product.findById(id)
+    const products = await Product.findById(id);
     res.status(200).json(products);
   } catch (err) {
     console.log(err);
   }
-}
-
+};
 
 const createProduct = async (req, res) => {
   const { name, price, image, description } = req.body;
@@ -73,36 +84,27 @@ const createProduct = async (req, res) => {
 const updateProduct = async (req, res) => {
   const { _id } = req.params;
   const { name, price, image, description } = req.body;
-console.log(name)
+  console.log(name);
   try {
     if (name) {
-
-      var product = await Product.findByIdAndUpdate(
-        _id, { $set: { name } }
-
-      )
+      var product = await Product.findByIdAndUpdate(_id, { $set: { name } });
     }
     if (price) {
-      var product = await Product.findByIdAndUpdate(
-        _id, { $set: { price } }
-      )
+      var product = await Product.findByIdAndUpdate(_id, { $set: { price } });
     }
     if (image) {
-      var product = await Product.findByIdAndUpdate(
-        _id, { $set: { image } }
-      )
+      var product = await Product.findByIdAndUpdate(_id, { $set: { image } });
     }
     if (description) {
-      var product = await Product.findByIdAndUpdate(
-        _id, { $set: { description } }
-      )
+      var product = await Product.findByIdAndUpdate(_id, {
+        $set: { description },
+      });
     }
     res.status(200).send(product);
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 };
-
 
 const updateRecipes = async (req, res) => {
   const { _id } = req.params;
@@ -149,8 +151,6 @@ const deleteProduct = async (req, res) => {
   }
 };
 
-
-
 module.exports = {
   getAll,
   getId,
@@ -159,4 +159,5 @@ module.exports = {
   updateRecipes,
   removeRecipes,
   deleteProduct,
+  getAllFilter,
 };
--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_PRODUCTS,GET_ALL_PRODUCTS, GET_ID_PRODUCTS, ADD_PRODUCTS, ADD_PRODUCTS_CART, DELETE_PRODUCTS_CART, CHANGE_FROM_CART, CLEAN_PRODUCT, ADD_COUNT_PROD} from "./actionsTypes";
+import { GET_PRODUCTS,GET_ALL_PRODUCTS, GET_ID_PRODUCTS, ADD_PRODUCTS, ADD_PRODUCTS_CART, DELETE_PRODUCTS_CART, CHANGE_FROM_CART, CLEAN_PRODUCT, ADD_COUNT_PROD, GET_ALL_FILTERS} from "./actionsTypes";
 
 
 
@@ -15,7 +15,7 @@ export const geTAllProducts = (page)=>{
     // console.log(products);
   };
 };
-
+//&filter=${filter}&${filter}=${valor}
 export const getAll = (name)=> {
   return async (dispatch)=>{
     const products = await axios.get(`${import.meta.env.VITE_URL}/products?search=${name}`)
@@ -26,7 +26,16 @@ export const getAll = (name)=> {
     // console.log(products);
   };
 }
-
+export const getAllFilters = (filter,valor)=> {
+  return async (dispatch)=>{
+    const products = await axios.get(`${import.meta.env.VITE_URL}/products/filter?filter=${filter}&${filter}=${valor}`)
+    dispatch({
+      type: GET_ALL_FILTERS,
+      payload: products.data,
+    });
+    // console.log(products);
+  };
+}
 export const getIdProducts = (payload)=>{
   return async (dispatch)=>{
     const products = await axios.get(`${import.meta.env.VITE_URL}/products/id/${payload}`)
