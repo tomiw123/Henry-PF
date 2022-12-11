36340dd6ceea546d72878bdfb3947c9b56d04df4
tomiw123 <tomacowalker@gmail.com>
2022-12-11 18:38:02 -0300

    arr

--- client/src/component/Carrito/Carrito.jsx
+++ client/src/component/Carrito/Carrito.jsx
@@ -4,146 +4,181 @@ import { useState } from "react";
 import * as BsIcons from "react-icons/bs";
 import * as GrIcons from "react-icons/gr";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteFromCart, addCount, payment ,addToCart} from "../../redux/actions/actions";
-import s from './Carrito.module.css';
+import {
+  deleteFromCart,
+  addCount,
+  payment,
+  addToCart,
+} from "../../redux/actions/actions";
+import s from "./Carrito.module.css";
 
 const Carrito = () => {
-    const [openedCart, setCart] = useState(false);
-    const dispatch = useDispatch();
-    const [total, setTotal] = useState(0);
-    const [count, setCount] = useState(0)
-    const openCart = ()=> {
-        setCart(!openedCart)
+  const [openedCart, setCart] = useState(false);
+  const dispatch = useDispatch();
+  const [total, setTotal] = useState(0);
+  const [count, setCount] = useState(0);
+  const openCart = () => {
+    setCart(!openedCart);
+  };
+  let cart = useSelector((state) => state.cart);
+
+  let newCart = [];
+  const [carritoVacio, setCarritoVacio] = useState(false);
+  useEffect(() => {
+    if (!newCart.length) {
+      setCarritoVacio(false);
+    } else {
+      setCarritoVacio(true);
     }
-    let cart = useSelector((state)=> state.cart);
+  });
 
-    let newCart = [];
-    const [carritoVacio, setCarritoVacio] = useState(false)
-    useEffect(()=> {
-        if(!newCart.length){
-            setCarritoVacio(false);
-        }else {
-            setCarritoVacio(true)
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].name) {
+      newCart.push(cart[i]);
+    }
+  }
+  window.localStorage.setItem("carrito", "hola");
+  if (newCart.length) {
+    window.localStorage.setItem("carrito", JSON.stringify(newCart));
+  } else {
+    let carritoStorage = window.localStorage.getItem("carrito");
+    console.log(carritoStorage);
+    if (carritoStorage !== "hola") {
+      let carritoStorageArray = JSON.parse(
+        window.localStorage.getItem("carrito")
+      );
+      if (carritoStorageArray.length) {
+        for (let i = 0; i < carritoStorageArray.length; i++) {
+          dispatch(addToCart(carritoStorageArray[i]));
         }
-    })
+      }
+    }
+  }
 
+  useEffect(() => {
+    let suma = 0;
     for (let i = 0; i < cart.length; i++) {
-        if(cart[i].name){
-            newCart.push(cart[i])
-        }
+      if (cart[i].quantity) {
+        suma = suma + cart[i].price * cart[i].quantity;
+      }
     }
-    if(newCart.length){
-        window.localStorage.setItem('carrito', JSON.stringify(newCart))
-    }else {
-        let carritoStorage = window.localStorage.getItem('carrito');
-        console.log(carritoStorage)
-        if(carritoStorage !== "hola"){
-            let carritoStorageArray = JSON.parse(window.localStorage.getItem('carrito'));
-            if(carritoStorageArray.length){
-                    for (let i = 0; i < carritoStorageArray.length; i++) {
-                            dispatch(addToCart(carritoStorageArray[i]))
-                        }
-                    }
-        }
-            }
-            
-    useEffect(()=>{
-        let suma = 0;
-        for (let i = 0; i < cart.length; i++) {
-            if(cart[i].quantity){
-                suma = suma + cart[i].price*cart[i].quantity
-            }
-        }
-        setTotal(suma)
-    })
-    
-    const deleteProd = (id)=> {
-        setTimeout(()=>{
-            dispatch(deleteFromCart(id))
-        }, 50)
-        if(newCart.length === 1 ){
-        window.localStorage.setItem('carrito', "hola")
-        }
-    }
-    
-    const sumarCantProd = (id)=> {
-        let obj = cart.find(p => p.id === id)
-        let lugar = cart.indexOf(obj)
-        let cantidad= obj.quantity + 1
-        setTimeout(()=> {
-            dispatch(addCount({cantidad, lugar}))
-        },30)
-    }
-    const restarCantProd = (id)=> {
-        let obj = cart.find(p => p.id === id)
-        let lugar = cart.indexOf(obj)
-        if(obj.quantity > 1){
-            obj= {
-                    ...obj,
-                    quantity: obj.quantity - 1
-                }
-        }
-        let cantidad= obj.quantity
-        setTimeout(()=> {
-            dispatch(addCount({cantidad, lugar}))
-        },30)
-    }
+    setTotal(suma);
+  });
 
-    const finalizarCompra = (newCart)=>{
-        let carritoFinal = [];
-        for (let i = 0; i < newCart.length; i++) {
-            let obj = {name: newCart[i].name, price: newCart[i].price, id: newCart[i].id, cant: newCart[i].quantity}
-            carritoFinal.push(obj)
-        }
-         dispatch(payment(carritoFinal)).then((e)=> window.location.replace(e))
+  const deleteProd = (id) => {
+    setTimeout(() => {
+      dispatch(deleteFromCart(id));
+    }, 50);
+    if (newCart.length === 1) {
+      window.localStorage.setItem("carrito", "hola");
     }
+  };
 
+  const sumarCantProd = (id) => {
+    let obj = cart.find((p) => p.id === id);
+    let lugar = cart.indexOf(obj);
+    let cantidad = obj.quantity + 1;
+    setTimeout(() => {
+      dispatch(addCount({ cantidad, lugar }));
+    }, 30);
+  };
+  const restarCantProd = (id) => {
+    let obj = cart.find((p) => p.id === id);
+    let lugar = cart.indexOf(obj);
+    if (obj.quantity > 1) {
+      obj = {
+        ...obj,
+        quantity: obj.quantity - 1,
+      };
+    }
+    let cantidad = obj.quantity;
+    setTimeout(() => {
+      dispatch(addCount({ cantidad, lugar }));
+    }, 30);
+  };
 
-    return (
-        <div className={s.cart}>
-            <div className={s.carritoNum}>
-                <BsIcons.BsCartFill onClick={openCart} className={s.carrito}/>
-                <p className={carritoVacio? s.numCarrito : s.sinNum}>{newCart.length}</p>
-            </div>
-            <div onClick={openCart} className={openedCart? s.backToHome : s.none}></div>
-            <div className={openedCart? s.openCart: s.closeCart}>
-                <div className={s.cross}>
-                    <GrIcons.GrClose className={s.close} onClick={openCart}/>
-                </div>
-                        <ul className={s.lista}>  
-                            <li className={s.listObj}>Producto</li>
-                            <li className={s.listUni}>Unid.</li>
-                            <li className={s.listObj}>Precio</li>
-                        </ul>
-                    {cart?.map(p=> {
-                        if(p.name){
-                            return (
-                                <div className={s.miniProd} key={p.id}>
-                                    <img src={p.image} alt="" className={s.image} />
-                                    <div className={s.prod}>{p.name}</div>
-                                    <div className={s.counter}>
-                                        <button className={s.contador} onClick={()=>restarCantProd(p.id)}>-</button>
-                                        <div className={s.prod}>{p.quantity}u</div>
-                                        <button className={s.contador} onClick={()=> sumarCantProd(p.id)}>+</button>
-                                    </div>
-                                    <div className={s.prod}>${p.price*p.quantity},00</div>
-                                    <button className={s.boton} onClick={()=> deleteProd(p.id)}>X</button>
-                                </div>
-                            )
-                        }
-                    })}
-                <div className={s.total}>
-                    <h3>Total</h3>
-                    <h3>$ {total},00 </h3>
-                </div>
-                
-                <div className={s.finalizar}>
-                    <button className={s.button} onClick={()=> {finalizarCompra(newCart)}}>Finalizar compra</button>
-                </div>
-                
-            </div>
+  const finalizarCompra = (newCart) => {
+    let carritoFinal = [];
+    for (let i = 0; i < newCart.length; i++) {
+      let obj = {
+        name: newCart[i].name,
+        price: newCart[i].price,
+        id: newCart[i].id,
+        cant: newCart[i].quantity,
+      };
+      carritoFinal.push(obj);
+    }
+    dispatch(payment(carritoFinal)).then((e) => window.location.replace(e));
+  };
+
+  return (
+    <div className={s.cart}>
+      <div className={s.carritoNum}>
+        <BsIcons.BsCartFill onClick={openCart} className={s.carrito} />
+        <p className={carritoVacio ? s.numCarrito : s.sinNum}>
+          {newCart.length}
+        </p>
+      </div>
+      <div
+        onClick={openCart}
+        className={openedCart ? s.backToHome : s.none}
+      ></div>
+      <div className={openedCart ? s.openCart : s.closeCart}>
+        <div className={s.cross}>
+          <GrIcons.GrClose className={s.close} onClick={openCart} />
         </div>
-    )
-}
+        <ul className={s.lista}>
+          <li className={s.listObj}>Producto</li>
+          <li className={s.listUni}>Unid.</li>
+          <li className={s.listObj}>Precio</li>
+        </ul>
+        {cart?.map((p) => {
+          if (p.name) {
+            return (
+              <div className={s.miniProd} key={p.id}>
+                <img src={p.image} alt="" className={s.image} />
+                <div className={s.prod}>{p.name}</div>
+                <div className={s.counter}>
+                  <button
+                    className={s.contador}
+                    onClick={() => restarCantProd(p.id)}
+                  >
+                    -
+                  </button>
+                  <div className={s.prod}>{p.quantity}u</div>
+                  <button
+                    className={s.contador}
+                    onClick={() => sumarCantProd(p.id)}
+                  >
+                    +
+                  </button>
+                </div>
+                <div className={s.prod}>${p.price * p.quantity},00</div>
+                <button className={s.boton} onClick={() => deleteProd(p.id)}>
+                  X
+                </button>
+              </div>
+            );
+          }
+        })}
+        <div className={s.total}>
+          <h3>Total</h3>
+          <h3>$ {total},00 </h3>
+        </div>
+
+        <div className={s.finalizar}>
+          <button
+            className={s.button}
+            onClick={() => {
+              finalizarCompra(newCart);
+            }}
+          >
+            Finalizar compra
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Carrito;
