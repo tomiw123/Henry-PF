94e634dc67415490c38126bda0a93689425eb223
lceras <luisfcss1102@hotmail.com>
2022-12-13 06:04:09 -0500

    a

--- client/src/component/ProductDetail/CardDetailProduct.jsx
+++ client/src/component/ProductDetail/CardDetailProduct.jsx
@@ -1,70 +1,77 @@
-import React, { useState, useEffect } from 'react'
-import Style from './CardDetailProduct.module.css'
+import React, { useState, useEffect } from "react";
+import Style from "./CardDetailProduct.module.css";
 import { useDispatch, useSelector } from "react-redux";
-import { AiFillStar } from 'react-icons/ai'
+import { AiFillStar } from "react-icons/ai";
 import { IconContext } from "react-icons";
-import { useParams } from 'react-router-dom';
-import { addToCart, changeFromCart, getIdProducts, cleanProduct } from '../../redux/actions/actions';
+import { useParams } from "react-router-dom";
+import {
+  addToCart,
+  changeFromCart,
+  getIdProducts,
+  cleanProduct,
+} from "../../redux/actions/actions";
+import { Oval } from "react-loader-spinner";
 
 const CardDetailProduct = () => {
-
   const dispatch = useDispatch();
-  const paramsId = useParams()
-  const Product = useSelector(state => state.product);
+  const paramsId = useParams();
+  const Product = useSelector((state) => state.product);
   // console.log(Product)
-  const [loader, setLoader] = useState(true)
-  setTimeout( ()=> {setLoader(false)}, 800)
-  useEffect(()=>{if(Product.length === 0){
-                  dispatch(getIdProducts(paramsId.id))
-                  }
-                  else{
-                   if(Product.id !== paramsId.id){
-                    dispatch(cleanProduct())
-                    dispatch(getIdProducts(paramsId.id))
-                   }
-                  } },[dispatch,paramsId.id])
-
+  const [loader, setLoader] = useState(true);
+  setTimeout(() => {
+    setLoader(false);
+  }, 800);
+  useEffect(() => {
+    if (Product.length === 0) {
+      dispatch(getIdProducts(paramsId.id));
+    } else {
+      if (Product.id !== paramsId.id) {
+        dispatch(cleanProduct());
+        dispatch(getIdProducts(paramsId.id));
+      }
+    }
+  }, [dispatch, paramsId.id]);
 
   const [counter, setCounter] = useState(1);
 
   function handleMax() {
-    setCounter(counter + 1)
+    setCounter(counter + 1);
   }
 
   function handleMin() {
-    if(counter > 1){
-      setCounter(counter - 1)
+    if (counter > 1) {
+      setCounter(counter - 1);
     }
   }
 
-  const cart = useSelector((state)=> state.cart)
+  const cart = useSelector((state) => state.cart);
   // const [cart, setCart] = useState([]);
 
-  const addProd = (id, name, image, price, counter)=> {
-    let yaEsta = cart.find(p => p.name === name);
-    if(!yaEsta){
-      const obj = {id, name, image,price, quantity: counter}
-      setTimeout(()=> {
-        dispatch(addToCart(obj))
-      },50)
-      alert(`${name} agregado al carrito`)
+  const addProd = (id, name, image, price, counter) => {
+    let yaEsta = cart.find((p) => p.name === name);
+    if (!yaEsta) {
+      const obj = { id, name, image, price, quantity: counter };
+      setTimeout(() => {
+        dispatch(addToCart(obj));
+      }, 50);
+      alert(`${name} agregado al carrito`);
     } else {
-      yaEsta={
+      yaEsta = {
         ...yaEsta,
-        quantity: yaEsta.quantity + counter
-      }
-      setTimeout(()=> {
-        dispatch(changeFromCart(yaEsta))
-      }, 50)
+        quantity: yaEsta.quantity + counter,
+      };
+      setTimeout(() => {
+        dispatch(changeFromCart(yaEsta));
+      }, 50);
     }
-  }
+  };
 
   return (
     <div className={Style.CardProduct}>
       <div className={Style.Container2}>
         <div className={Style.ImgCont}>
           <div className={Style.Image}>
-          {loader? (
+            {loader ? (
               <Oval
                 height={100}
                 width={300}
@@ -72,16 +79,17 @@ const CardDetailProduct = () => {
                 wrapperStyle={{}}
                 wrapperClass=""
                 visible={true}
-                ariaLabel='oval-loading'
+                ariaLabel="oval-loading"
                 secondaryColor="black"
                 strokeWidth={2}
                 strokeWidthSecondary={2}
               />
-              ): (
-                <img className={Style.img} src={Product.image}  alt="" />
-              )}
+            ) : (
+              <img className={Style.img} src={Product.image} alt="" />
+            )}
           </div>
-          <div className={Style.Container}>{/********** */}
+          <div className={Style.Container}>
+            {/********** */}
             <div className={Style.Title}>
               <h1>{Product.name}</h1>
             </div>
@@ -99,23 +107,40 @@ const CardDetailProduct = () => {
             <div className={Style.Price}>${Product.price},00</div>
             <div className={Style.ContButtom}>
               <div className={Style.Cont}>
-                <button className={Style.btnmaxmin} onClick={handleMin}>-</button>
+                <button className={Style.btnmaxmin} onClick={handleMin}>
+                  -
+                </button>
                 <div className={Style.Num}>{counter}</div>
-                <button className={Style.btnmaxmin} onClick={handleMax}>+</button>
+                <button className={Style.btnmaxmin} onClick={handleMax}>
+                  +
+                </button>
               </div>
               <div className={Style.Compra}>
-                <button className={counter===0 ? Style.btnDis : Style.btn} onClick={()=> {addProd(Product._id ,Product.name, Product.image, Product.price, counter)}}>Agregar al Carrito</button>
+                <button
+                  className={counter === 0 ? Style.btnDis : Style.btn}
+                  onClick={() => {
+                    addProd(
+                      Product._id,
+                      Product.name,
+                      Product.image,
+                      Product.price,
+                      counter
+                    );
+                  }}
+                >
+                  Agregar al Carrito
+                </button>
               </div>
             </div>
           </div>
         </div>
         <div className={Style.Description}>
-          <p className='description'>{Product.description}</p>
+          <p className="description">{Product.description}</p>
         </div>
         {/* dejar un apartado para poner abajo las recetas que se pueden hacer con el producto*/}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardDetailProduct
+export default CardDetailProduct;
