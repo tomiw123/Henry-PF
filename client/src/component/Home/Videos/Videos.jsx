0e5b5a33579d01957d3825c4c02dc3d58d919381
lceras <luisfcss1102@hotmail.com>
2022-12-14 06:42:32 -0500

    diseño responsive en medida de 379 * 667

--- client/src/component/Home/Videos/Videos.jsx
+++ client/src/component/Home/Videos/Videos.jsx
@@ -3,27 +3,24 @@ import ReactPlayer from 'react-player'
 
 
 
+
 const Video = () => {
 
 
 
     return (
-      <div className='flex justify-around text-center '>
+      <div className='flex justify-center text-center '>
+
         <ReactPlayer
+          
           url='https://www.youtube.com/watch?v=A2Ni1WhwnW8&ab_channel=DosDagasFogoneros'
-        //   className='react-player'
-          className=" my-10 w-full h-80   min-h-max hover:scale-110 transition duration-300 ease-in-out"
-          //playing
-        //   width='100%'
-        //   height='100%'
+          className="my-10 min-h-max hover:scale-110 transition duration-300 ease-in-out"
         />
+
         <ReactPlayer
           url='https://www.youtube.com/shorts/J9KS_yKMI-A'
-        //   className='react-player'
-          className=" my-10 w-full h-80  min-h-max hover:scale-110 transition duration-300 ease-in-out"
-         // playing
-        //   width='100%'
-        //   height='100%'
+          // className=" my-10 min-h-max hover:scale-110 transition duration-300 ease-in-out "
+          className="my-10 min-h-max hover:scale-110 transition duration-300 ease-in-out "
         />
       </div>
     );
