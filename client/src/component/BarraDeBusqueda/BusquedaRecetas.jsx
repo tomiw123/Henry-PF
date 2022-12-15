51c35278b624089323b42f705d03ea8a17c20312
lceras <luisfcss1102@hotmail.com>
2022-12-14 23:31:32 -0500

    agregando estilos css

--- client/src/component/BarraDeBusqueda/BusquedaRecetas.jsx
+++ client/src/component/BarraDeBusqueda/BusquedaRecetas.jsx
@@ -1,41 +1,60 @@
 import React from "react";
 import { useState } from "react";
-import s from './BusquedaRecetas.module.css'
-import {useDispatch } from "react-redux";
+import s from "./BusquedaRecetas.module.css";
+import { useDispatch } from "react-redux";
 import { getAllRecipes, getByName } from "../../redux/actions/recipesActions";
+import {AiOutlineCloseCircle} from 'react-icons/ai'
+import { IconContext } from "react-icons/lib";
 
+const BusquedaRecetas = () => {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
 
-const BusquedaRecetas = ()=> {
-    const [input, setInput] = useState('');
-    const dispatch = useDispatch();
-
-    const inputHandler = (e) => {
-        if(e.target.value === ''){
-            clearInput()
-        }
-            setInput(e.target.value);
-        // console.log(input)
-      };
-    const submit = ()=> {
-        dispatch(getByName(input));
+  const inputHandler = (e) => {
+    if (e.target.value === "") {
+      clearInput();
     }
+    setInput(e.target.value);
+    // console.log(input)
+  };
+  const submit = (event) => {
+    event.preventDefault();
+    dispatch(getByName(input));
+    setInput("");
+  };
+
+  const clearInput = () => {
+    dispatch(getAllRecipes());
     
-    const handleSubmit = (event)=>  {
-        event.preventDefault();
-      }
-    const clearInput = ()=> {
-        dispatch(getAllRecipes())
-        setInput('')
-    }
-    return (
-        <form onSubmit={(e)=> handleSubmit(e)}>
-            <div className={s.search}>
-                <input type="text" name="Search" placeholder='Search...' className={s.input} value={input} onChange={(e) => inputHandler(e) }/>
-                <button className={s.button} onClick={()=> clearInput()}>X</button>
-                <button className={s.submit} onClick={()=> {submit()}}>Search</button>
-            </div>
-        </form>
-    )
-}
+  };
+  return (
+    <form onSubmit={(e) => submit(e)}>
+      <div className={s.search}>
+        <div className={s.test}>
+        <input
+          type="text"
+          name="Search"
+          placeholder="Search..."
+          className={s.input}
+          value={input}
+          onChange={(e) => inputHandler(e)}
+          autoComplete="off"
+        />
+        <IconContext.Provider value={{size: "30px"}}>
+        <AiOutlineCloseCircle  onClick={() => clearInput()}>
+        </AiOutlineCloseCircle>
+        </IconContext.Provider>
+        </div>
+
+        <div className={s.test2}>
+        <button
+          className={s.submit}>
+          Search
+        </button>
+        </div>
+      </div>
+    </form>
+  );
+};
 
 export default BusquedaRecetas;
--- client/src/component/Home/Videos/Videos.jsx
+++ client/src/component/Home/Videos/Videos.jsx
@@ -1,32 +1,23 @@
-import React, { Component } from 'react'
-import ReactPlayer from 'react-player'
-
-
+import React, { Component } from "react";
+import ReactPlayer from "react-player";
+import style from './Video.module.css'
 
 
 const Video = () => {
+  return (
+    <div className="flex justify-center text-center ">
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=A2Ni1WhwnW8&ab_channel=DosDagasFogoneros"
+        className="my-10 hover:scale-110 transition duration-300 ease-in-out mr-20"
+        width="30%"
+      />
+      <ReactPlayer
+        url="https://www.youtube.com/shorts/J9KS_yKMI-A"
+        className="my-10  hover:scale-110 transition duration-300 ease-in-out"
+        width="30%"
+      />
+    </div>
+  );
+};
 
-
-
-    return (
-      <div className='flex justify-center text-center '>
-
-        <ReactPlayer
-          
-          url='https://www.youtube.com/watch?v=A2Ni1WhwnW8&ab_channel=DosDagasFogoneros'
-
-          className="my-10 min-h-max hover:scale-110 transition duration-300 ease-in-out"
-        />
-
-        <ReactPlayer
-          url='https://www.youtube.com/shorts/J9KS_yKMI-A'
-          // className=" my-10 min-h-max hover:scale-110 transition duration-300 ease-in-out "
-          className="my-10 min-h-max hover:scale-110 transition duration-300 ease-in-out "
-
-        />
-      </div>
-    );
-  }
-
-
-export default Video
+export default Video;
