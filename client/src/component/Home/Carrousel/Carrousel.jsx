9d2ff2899f73a0626c2e0e40ad84634416d63c41
Matias Carballo <98398164+MatiasCarballo@users.noreply.github.com>
2023-01-11 10:10:28 -0300

    Update Carrousel.jsx

--- client/src/component/Home/Carrousel/Carrousel.jsx
+++ client/src/component/Home/Carrousel/Carrousel.jsx
@@ -15,14 +15,14 @@ const Carrousel = () => {
     const handleDragStart = (e) => e.preventDefault();
 
     const items = [
-      <img src={asado} onDragStart={handleDragStart} role="presentation" className={style.image}/>,
+      <img src={achuras} onDragStart={handleDragStart} role="presentation" className={style.image}/>,
       <img src={img} onDragStart={handleDragStart} role="presentation" className={style.image}/>,
+      <img src={asado} onDragStart={handleDragStart} role="presentation" className={style.image}/>,
         // <img src="https://irp-cdn.multiscreensite.com/1e15d305/MOBILE/jpg/1622772-el-almacen-parrilla-banner.jpg" onDragStart={handleDragStart} role="presentation" className={style.image}/>,
         // <img src="https://www.momentosfriko.com/Portals/MomentosFriko/Images/tips-asado-banner_blog.jpg?ver=2020-06-11-144643-457" onDragStart={handleDragStart} role="presentation" className={style.image} />,
         // <img src="https://olivetto.com.co/wp-content/uploads/Asados-banner.jpg?x16765" onDragStart={handleDragStart} role="presentation" className={style.image}/>,
         <img src={cuchillos} onDragStart={handleDragStart} role="presentation" className={style.image}/>,
         <img src={achuras2} onDragStart={handleDragStart} role="presentation" className={style.image}/>,
-        <img src={achuras} onDragStart={handleDragStart} role="presentation" className={style.image}/>,
       ];
 
   return (
